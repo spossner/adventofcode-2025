189b6c1627e3d9bf6e74fa6ccf11bd2c37dd4c31
Sebastian Possner <github@possner.de>
2025-12-01 21:25:29 +0100

    initial commit from day 1 2025

---
+++ README.md
@@ -0,0 +1,49 @@
+# Advent of code 2025
+
+Again.. this year hust 12 puzzles.. at least state now
+
+## PRE-CONFIGURATION
+- update YEAR in _.py
+- setup interpreter
+
+## 3rd party libs
+- python-dotenv
+- requests
+- aoc (-> see local if you want to use local implementation)
+
+## New day
+
+Clone the _.py blueprint for every new day.
+
+## Puzzle input
+
+Puzzle input is automatically fetched when the valid AOC cookie is specified in .env file. Note that the script must be named with the
+number of the day - e.g. 7.py or 07.py for day 7.
+Therefore
+
+- clone the .env.sample into .env
+- log into ACO with your browser
+- search for `session` cookie
+- copy & paste the value into .env file
+
+## AOC
+
+AOC is my own library with some helpful classes and functions for AOC.
+
+### LOCAL
+
+In order to use the local installation of aoc-lib, add the path to aoc project as
+custom paths in interpreter settings. Therefore
+
+- open project settings
+- Python interpreter
+- Open the dropdown of the selected interpreter (e.g. Python 3.9 (2022))
+- choose Show all...
+- switch to the tree icon "Show interpreter paths"
+- add a custom path to aoc project
+
+## PUZZLES
+
+### Day 10
+
+CPU to control a cathode-ray
---
+++ _.py
@@ -0,0 +1,95 @@
+import os
+import sys
+from os.path import exists
+
+import requests
+from aoc import get_ints
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# select one of
+DEV, PART2 = True, False  # DEV PART1
+# DEV, PART2 = False, False # PROD PART1
+# DEV, PART2 = True, True # DEV PART2
+# DEV, PART2 = False, True # PROD PART2
+
+STRIP = True
+SPLIT_LINES = True
+SPLIT_CHAR = None
+GET_INTS = False
+DATA = None
+AOC_SESSION = os.environ.get("AOC_SESSION")
+YEAR = 2025
+
+
+class Solution:
+    def __init__(self, data):
+        if data and STRIP and type(data) is str:
+            data = data.strip()
+        if data and SPLIT_LINES and type(data) is str:
+            data = data.splitlines()
+        if data and SPLIT_CHAR is not None:
+            if SPLIT_CHAR == "":
+                data = [list(row) for row in data] if SPLIT_LINES else list(data)
+            else:
+                data = (
+                    [row.split(SPLIT_CHAR) for row in data]
+                    if SPLIT_LINES
+                    else data.split(SPLIT_CHAR)
+                )
+        if GET_INTS:
+            if type(data) is str:
+                data = get_ints(data)
+            else:
+                data = list(
+                    map(
+                        lambda e: get_ints(e)
+                        if type(e) is str
+                        else [get_ints(v) for v in e],
+                        data,
+                    )
+                )
+        self.data = self.parse_data(data)
+
+    def parse_data(self, data):
+        return data
+
+    def first_part(self):
+        result = 0
+
+        for row in self.data:
+            print(row)
+
+        return result
+
+    def second_part(self):
+        return self.first_part()
+
+
+if __name__ == "__main__":
+    script = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    if "-" in script:
+        script = script.split("-")[0]
+
+    DATA_URL = f"https://adventofcode.com/{YEAR}/day/{int(script)}/input"
+
+    if not DATA:
+        file_name = (
+            f"{script}-dev{DEV if type(DEV) is not bool else ''}.txt"
+            if DEV
+            else f"{script}.txt"
+        )
+        if exists(file_name):
+            with open(file_name) as f:
+                DATA = f.read()
+        elif AOC_SESSION and DATA_URL:
+            DATA = requests.get(
+                DATA_URL, headers={"Cookie": f"session={AOC_SESSION}"}
+            ).text
+            with open(file_name, "w") as f:
+                f.write(DATA)
+
+    print(f"DAY {int(script)}")
+    s = Solution(DATA)
+    print("RESULT", s.first_part() if not PART2 else s.second_part())
